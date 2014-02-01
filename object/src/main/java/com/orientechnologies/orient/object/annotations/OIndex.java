15d35ba36e0166c840e03ea3e4f1413179e26ecc
spostma73 <sanderpostma@yahoo.com>
2014-02-01 22:34:48 +0100

    Create OIndex.java

---
+++ object/src/main/java/com/orientechnologies/orient/object/annotations/OIndex.java
@@ -0,0 +1,19 @@
+package com.orientechnologies.orient.object.annotations;
+
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Sander Postma (s.postma@magsoft.nl)
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface OIndex
+{
+    String name() default "";
+    OClass.INDEX_TYPE indexType();
+    OType keyType() default OType.INHERITED;
+}
